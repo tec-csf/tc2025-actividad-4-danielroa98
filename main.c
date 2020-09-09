2ee2298b4cbdd610040855528a0d725e61f06bfb
Daniel Roa <daniel.roa98@gmail.com>
2020-09-08 19:08:01 -0500

    Started PID and PPID
    Must fix infinite loop.

--- main.c
+++ main.c
@@ -14,15 +14,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 void easter_egg();
 void instruction();
 
+typedef struct procceses{
+    int childP;
+    int avg;
+} procDat;
+
 int main(int argc, char * const *argv)
 {
     char * numberVal = NULL;
-    int dat, itSup = 0, convNumber;
-
+    int dat, itSup = 0, convNumber = 0, contPID = 0;
+    pid_t pid;
+    
+    //Error value - opterr se convierte en ? cuando marca un error
     opterr = 0;
 
     while ((dat = getopt(argc, argv, "n:em")) != -1){
@@ -32,9 +40,8 @@ int main(int argc, char * const *argv)
         //Caso principal, donde el usuario insertó n números
         case 'n':
             numberVal = optarg;
-
+            //Convert char to int
             convNumber = atoi(numberVal);
-
             break;
         
         //En caso que el usuario quiera ver el manual de uso del programa
@@ -54,17 +61,44 @@ int main(int argc, char * const *argv)
         }
     }
 
-    if(itSup == 1){
-        instruction();
+    procDat *proc = (procDat *)malloc(sizeof(procDat) * convNumber);
+    procDat * fin = proc + convNumber;
+    procDat *cont = proc;
+
+    /* for (procDat *cont = proc; cont < fin; ++cont){
+        if(pid == -1){
+            printf("Error al crear al hijo\n");
+        }else if(pid == 0){
+
+        }
+    } */
+    
+    while (cont < fin && contPID < convNumber)
+    {
+        pid = fork();
+        if (pid == -1){
+
+            printf("Error al crear al hijo\n");
+
+        }else if(pid == 0){
+
+            printf("El proceso padre es %d y su hijo es %d\n", getpid(), getppid());
+
+        }
+        
     }
 
-    printf("Se insertó el valor numérico de %d\n", convNumber);
+    //printf("Se insertó el valor numérico de %d\n", convNumber);
+
+    if (itSup == 1){
+        instruction();
+    }
 
     return 0;
 }
 
 void easter_egg(){
-    printf("Congrats! You just found me!\n\n\nPlease mark me with a great grade!\n:)\n");
+    printf("Congrats! You just found me!\n\nPlease mark me with a great grade!\n:)\n");
 }
 
 void instruction(){
